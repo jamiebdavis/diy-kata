7753f9a71ca050af36d82bb9b79e771b04f0d1ee
Jamie Davis <jamied1985@gmail.com>
2018-06-05 21:12:02 +0100

    play around

--- __tests__/kata1.fizzBuzz.test.js
+++ __tests__/kata1.fizzBuzz.test.js
@@ -2,7 +2,7 @@ const { fizzBuzz } = require('../src');
 
 describe('fizzBuzz', () => {
   it('returns Fizz when passed a multiple of 3', () => {
-
+    expect(fizzBuzz(3).toBe("Buzz"))
   });
 
   it('returns Buzz when passed a multiple of 5', () => {
--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,7 @@
 const fizzBuzz = (number) => {
-
+ if (number % 3 === 0) {
+ return "Fizz"
+}
 }
 
 module.exports = fizzBuzz;
