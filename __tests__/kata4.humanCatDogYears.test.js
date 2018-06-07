7078931dbae18a7ea7d008cf4700293dee0cbf0f
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-07 19:47:30 +0100

    Refactor on humancatdog

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -3,5 +3,7 @@ const { humanCatDogYears } = require('../src');
 describe('humanCatDogYears', () => {
     it('returns array human, cat and dog age from human age input', () => {
       expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+      expect(humanCatDogYears(15)).toEqual([15, 76, 89]);
+      expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
     });
   });
