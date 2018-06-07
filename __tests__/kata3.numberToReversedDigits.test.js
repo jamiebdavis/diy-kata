0a6e7029b95a80689321caa7fbbf435e53278fbb
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-07 19:47:30 +0100

    Refactor on humancatdog

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -4,5 +4,6 @@ describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
     expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1]);
     expect(numberToReversedDigits(72906)).toEqual([6, 0, 9, 2, 7]);
+    expect(numberToReversedDigits(54321)).toEqual([1, 2, 3, 4, 5])
   });
 });
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,8 +1,20 @@
 const humanCatDogYears = (number) => {
-    const catAge = 24 + (4*(number-2));
-    const dogAge = 24 + (5*(number-2));
+    let catAge = 24 + (4 * (number - 2));
+    let dogAge = 24 + (5 * (number - 2));
 
-    return [number, catAge, dogAge]
+    if(number === 1) {
+      catAge = 15;
+      dogAge = 15;
+    }
+
+    if(number === 2) {
+      catAge = 24;
+      dogAge = 24;
+    }
+
+    else
+
+     return [number, catAge, dogAge]
 }
 
 module.exports = humanCatDogYears;
