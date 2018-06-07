5dd674d85ccd12c3c20e7a658ed52a58b15b7f9d
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-07 20:08:01 +0100

    Further tests added to reach destination

--- __tests__/kata5.reachDestination.test.js
+++ __tests__/kata5.reachDestination.test.js
@@ -4,5 +4,8 @@ describe('reachDestination', () => {
   it('returns string with estimated time of arrival', () => {
     expect(reachDestination(10, 5)).toEqual(2)
     expect(reachDestination(20, 4)).toEqual(5)
+    expect(reachDestination(1, 5)).toEqual(0.2)
+    expect(reachDestination(0, 4)).toEqual(0)
+    expect(reachDestination(3, 0)).toEqual(Infinity)
   });
 });
