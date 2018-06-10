44cc3fe5fcf8431075de9b3e13764212926bb30b
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-10 19:59:11 +0100

    remove joinNames code

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,14 +1,5 @@
 const joinNames = (namesObj) => {
-  [ {name: 'Bart'},
-    {name: 'Lisa'},
-    {name: 'Maggie'}]
-
-  let newString = ''
-
-  for(let i = 0; i < namesObj.length; i++);
-    newString += name.Obj[i].name + ","
 
 }
-  return namesObj
 
 module.exports = joinNames;
