5a7c4009b63b8130e12ea0e683c528cea470630c
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-12 20:54:10 +0100

    employees role complete

--- __tests__/kata7.getEmployerRole.test.js
+++ __tests__/kata7.getEmployerRole.test.js
@@ -2,6 +2,18 @@ const { getEmployerRole } = require('../src');
 
 describe('getEmployerRole', () => {
   it('returns the employee\'s role in the company', () => {
-    
+    const employees = [{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Resources Assistant'
+    }];
+
+    expect(getEmployerRole('Jenny', employees)).toBe('Sales Associate')
+
   });
 });
