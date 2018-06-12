eaac51ec249dd8184855c1262080570cda313ad9
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-12 20:09:39 +0100

    resolved merge conflict

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,12 +2,8 @@ const { numberToReversedDigits } = require('../src');
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-<<<<<<< HEAD
     expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1]);
     expect(numberToReversedDigits(72906)).toEqual([6, 0, 9, 2, 7]);
     expect(numberToReversedDigits(54321)).toEqual([1, 2, 3, 4, 5])
-=======
-    expect(numberToReversedDigits(12345)).toEqual([ 5, 4, 3, 2, 1 ]);
->>>>>>> 5d51813a7208f4860ab0f747ca9e30b45df8e407
   });
 });
