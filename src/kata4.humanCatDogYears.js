d5547e683bf9a8c3854d8c6c1774e9e115e267ac
Jamie Davis <jamied1985@gmail.com>
2018-06-21 10:18:25 +0100

    final Changes - ask Joe to check

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,19 +1,30 @@
 const humanCatDogYears = (number) => {
-    let catAge = 24 + (4 * (number - 2));
-    let dogAge = 24 + (5 * (number - 2));
 
-    if(number === 1) {
-      catAge = 15;
-      dogAge = 15;
-    }
-
-    if(number === 2) {
+  let dogAge;
+  let catAge;
+  let ageArray = []
+  
+  if (number === 1) {
+    ageArray.push(number)
+    catAge = 15;
+    ageArray.push(catAge)
+    dogAge = 15;
+    ageArray.push(dogAge)
+  }
+  else if (number === 2){
+      ageArray.push(number)
       catAge = 24;
+      ageArray.push(catAge)
       dogAge = 24;
-    }
-
-    else
-
-     return [number, catAge, dogAge]
+      ageArray.push(dogAge)    
+  }
+  else if (number >= 3) {
+      ageArray.push(number)
+      catAge = number * 4 + 24 - 8;
+      ageArray.push(catAge)
+      dogAge = number * 5 + 24 - 10;
+      ageArray.push(dogAge)
+  }
+  return ageArray
 }
 module.exports = humanCatDogYears;
